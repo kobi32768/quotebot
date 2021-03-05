0e29150d0fc0fb81088215f9d09238e8e2656bab
kobi32768 <kobi32768@gmail.com>
2021-03-05 18:58:31 +0900

    Bugfix

--- src/main/kotlin/io/github/kobi32768/quotebot/DiscordMessage.kt
+++ src/main/kotlin/io/github/kobi32768/quotebot/DiscordMessage.kt
@@ -1,6 +1,7 @@
 package io.github.kobi32768.quotebot
 
 import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.entities.ChannelType
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import java.awt.Color
 
@@ -25,30 +26,31 @@ class DiscordMessage {
         val channel = data.channel
         val message = data.message
         val event = data.event
-
-        val log = Logger()
-
+        
         val embed = EmbedBuilder()
             .setTitle("from: ${channel.name} / ${guild.name}")
             .setDescription(message.contentDisplay)
             .setTimestamp(message.timeCreated)
             .setColor(Color(238, 150, 181))
 
-        // When quote from other guild:
-        // * Can't get Member
-        if (isSameGuild(data)) {
+        // If get message from IDs, API don't get member
+        if (isSameGuild(data) && message.isFromType(ChannelType.TEXT)) {
             embed.setAuthor(
-                message.member!!.effectiveName, null,
+                getName(data),
+                null,
                 message.author.effectiveAvatarUrl)
         }
 
-        event.channel.sendMessage(embed.build()).queue()
-        log.printlog("Successfully referenced", State.SUCCESS, data)
+        event.channel
+            .sendMessage(embed.build())
+            .queue()
     }
 
-    fun languageChanger(event: MessageReceivedEvent) {
-        sendMessage("Select number\n```" +
-                "1. ja_JP: 日本語\n" +
-                "2. en_US: English```", event)
+    private fun getName(data: MessageData): String {
+        val message = data.message
+        if (message.member == null)
+            return message.author.name
+        else
+            return message.member!!.effectiveName
     }
 }
