99e37f4bc38444b85fc57b470110c68259c18cad
anatawa12 <anatawa12@icloud.com>
2022-08-18 12:23:34 +0900

    fix: CWD is used in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ application {
 }
 
 group   = "io.github.kobi32768"
-version = File("./src/main/resources/version.txt").readText()
+version = file("src/main/resources/version.txt").readText()
 
 val jar by tasks.getting(Jar::class) {
     manifest {
