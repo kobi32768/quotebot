23f9b2f31f87f7289821fc6c7cd2397c0d1d8e1c
anatawa12 <anatawa12@icloud.com>
2022-08-18 15:14:01 +0900

    fix: version name may include new line

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group   = "io.github.kobi32768"
-version = file("src/main/resources/version.txt").readText()
+version = file("src/main/resources/version.txt").readText().trim()
 
 val jar by tasks.getting(Jar::class) {
     manifest {
