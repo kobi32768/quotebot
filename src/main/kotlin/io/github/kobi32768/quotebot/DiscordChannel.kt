77ef203ee77e29e328580ca90fce6c0e888b486f
Sayamame-beans <61457993+Sayamame-beans@users.noreply.github.com>
2022-08-20 15:57:20 +0900

    refactor: Simplify type check
    https://github.com/kobi32768/quotebot/pull/28#discussion_r950605550

--- src/main/kotlin/io/github/kobi32768/quotebot/DiscordChannel.kt
+++ src/main/kotlin/io/github/kobi32768/quotebot/DiscordChannel.kt
@@ -2,11 +2,8 @@ package io.github.kobi32768.quotebot
 
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.GuildMessageChannel
-import net.dv8tion.jda.api.entities.NewsChannel
 import net.dv8tion.jda.api.entities.StandardGuildMessageChannel
-import net.dv8tion.jda.api.entities.TextChannel
 import net.dv8tion.jda.api.entities.ThreadChannel
-import net.dv8tion.jda.api.entities.VoiceChannel
 import net.dv8tion.jda.api.entities.channel.attribute.IAgeRestrictedChannel
 
 fun Guild.getQuotableChannelById(id: String): GuildMessageChannel? {
@@ -18,8 +15,8 @@ fun Guild.getQuotableChannelById(id: String): GuildMessageChannel? {
 
 fun GuildMessageChannel.isNSFW(): Boolean {
     return when (this) {
-        is TextChannel, is VoiceChannel, is NewsChannel -> {
-            (this as IAgeRestrictedChannel).isNSFW
+        is IAgeRestrictedChannel -> {
+            this.isNSFW
         }
         is ThreadChannel -> {
             (this.parentChannel as StandardGuildMessageChannel).isNSFW
